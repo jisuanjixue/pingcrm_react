947c8420d5af05298b6f74d28db346ff9f139f99
jisuanjixue <2078350663@qq.com>
2022-10-11 11:13:13 +0800

    add axios for CSRF protection

--- app/javascript/entrypoints/application.js
+++ app/javascript/entrypoints/application.js
@@ -3,57 +3,58 @@
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
 
-import '~/styles/application.css';
+import "~/styles/application.css";
 
-import React from 'react';
-import { render } from 'react-dom';
+import React from "react";
+import { render } from "react-dom";
 
-import { Inertia } from '@inertiajs/inertia';
-import Plausible from 'plausible-tracker';
+import { Inertia } from "@inertiajs/inertia";
+import Plausible from "plausible-tracker";
+import axios from "axios";
 
-import { createInertiaApp } from '@inertiajs/inertia-react';
-import { InertiaProgress } from '@inertiajs/progress';
+import { createInertiaApp } from "@inertiajs/inertia-react";
+import { InertiaProgress } from "@inertiajs/progress";
 
-import { ChakraProvider } from '@chakra-ui/react';
-import theme from './theme/theme';
+import { ChakraProvider } from "@chakra-ui/react";
+import theme from "./theme/theme";
 
-const plausibleUrl = document.querySelector(
-  'meta[name="plausible-url"]',
-).content;
+const plausibleUrl = document.querySelector('meta[name="plausible-url"]').content;
 if (plausibleUrl) {
   const plausible = Plausible({
     domain: document.querySelector('meta[name="app-host"]').content,
     apiHost: plausibleUrl,
   });
 
-  Inertia.on('navigate', () => {
+  Inertia.on("navigate", () => {
     plausible.trackPageview();
   });
 }
 
+document.addEventListener("DOMContentLoaded", () => {
+  const csrfToken = document.querySelector("meta[name=csrf-token]").content;
+  axios.defaults.headers.common["X-CSRF-Token"] = csrfToken;
+});
+
 InertiaProgress.init();
 
-const pages = import.meta.globEagerDefault('../Pages/**/*.tsx');
+const pages = import.meta.globEagerDefault("../Pages/**/*.tsx");
 
 createInertiaApp({
-  resolve: (name) => {
+  resolve: name => {
     const component = pages[`../Pages/${name}.tsx`];
-    if (!component)
-      throw new Error(
-        `Unknown page ${name}. Is it located under Pages with a .tsx extension?`,
-      );
+    if (!component) throw new Error(`Unknown page ${name}. Is it located under Pages with a .tsx extension?`);
 
     return import(`./Pages/${name}`);
   },
 
-  title: (title) => (title ? `${title} - Ping CRM` : 'Ping CRM'),
+  title: title => (title ? `${title} - Ping CRM` : "Ping CRM"),
 
   setup({ el, App, props }) {
     render(
       <ChakraProvider theme={theme} resetCSS={false}>
         <App {...props} />
       </ChakraProvider>,
-      el,
+      el
     );
   },
 });
